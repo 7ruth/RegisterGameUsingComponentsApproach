fe6697165964a477847dc8be839323dfe48c3447
Martin Terskin <martinas.terskin@gmail.com>
2016-04-04 19:27:47 -0400

    Completed the assignment, also added a feature where you can specify the number of registers you want to create

--- js/main.js
+++ js/main.js
@@ -1,28 +1,65 @@
-var entries = $("#entries");
+function Component() {
+  this.domElement = null;
 
-var form = $("form");
-var inputBox = $("#newEntry");
+  this.initialize = function(el) {
+  this.domElement = $('<div class="register"><div class="ticket"><h1>Thank you!</h1><table><tbody class="entries"></tbody><tfoot><tr><th> Total </th> <th class="total"> $0.00 </th></tr></tfoot></table></div><form class="entry"> <input class="newEntry" autofocus placeholder="How much?"></form></div>');
+  el.append(this.domElement);
+  };
 
-var totalEl = $("#total");
-var total = 0.00;
 
-form.on("submit", updateRegister);
+  this.render = function () {
 
-function updateRegister() {
-  event.preventDefault();
+    var entries = this.domElement.find('.entries');
 
-  // get the amount and clear the box
-  var amount = parseFloat(inputBox.val());
-  inputBox.val("");
+    var form = this.domElement.find('form');
+    var inputBox = this.domElement.find('.newEntry');
 
-  // make the new element and add it to the list of entries
-  var newEl = $("<tr>");
-  newEl.append("<td></td>");
-  newEl.append("<td>" + "$" + amount.toFixed(2) + "</td>");
+    var totalEl = this.domElement.find('.total');
+    var total = 0.00;
 
-  entries.append(newEl);
 
-  // update the total
-  total = total + amount;
-  totalEl.text("$" + total.toFixed(2));
+    form.on("submit", updateRegister);
+
+    function updateRegister() {
+      event.preventDefault();
+
+      // get the amount and clear the box
+      var amount = parseFloat(inputBox.val());
+      inputBox.val("");
+
+      // make the new element and add it to the list of entries
+      var newEl = $("<tr>");
+      newEl.append("<td></td>");
+      newEl.append("<td>" + "$" + amount.toFixed(2) + "</td>");
+
+      entries.append(newEl);
+
+      // update the total
+      total = total + amount;
+      totalEl.text("$" + total.toFixed(2));
+    }
+
+
+  };
+
 }
+
+$('.start').submit(function(){
+    event.preventDefault();
+    var registerCounter = $('.registerCounter')[0].value;
+
+    $('.start').remove();
+
+    for (var i = 0; i<registerCounter; i++){
+        var registers = [];
+        registers[i] = new Component();
+        registers[i].initialize($('body'));
+        registers[i].render();
+    }
+
+});
+
+
+// var register_1 = new Component();
+// register_1.initialize($('body'));
+// register_1.render();
